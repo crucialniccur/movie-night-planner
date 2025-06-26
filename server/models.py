3562bdd6dedfe67d4c4063a939dd3da6cc660299
Mati Joseph <crucialniccur@gmail.com>
2025-06-26 10:31:42 +0300

    Define User model with relationships and serialization rules

--- server/models.py
+++ server/models.py
@@ -4,3 +4,13 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    reviews = db.relationship('Review', backref='user', lazy=True)
+    events = db.relationship('UserEvent', backref='user', lazy=True)
+
+    serialize_rules = ('-reviews.user', '-events.user')

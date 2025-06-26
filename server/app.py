e7215b22ae571108533e1ae7ede8e1216db0b3a6
Mati Joseph <crucialniccur@gmail.com>
2025-06-26 10:46:32 +0300

    Implement EventList resource with GET and POST methods for event management

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from flask import Flask
 
 # Remote library imports
 from flask import request
@@ -18,6 +17,25 @@ from models import User, Event, Review, UserEvent
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+
+class EventList(Resource):
+    def get(self):
+        events = [event.to_dict() for event in Event.query.all()]
+        return events, 200
+
+    def post(self):
+        data = request.get_json()
+        try:
+            event = Event(
+                title=data['title'],
+                date=datetime.strptime(data['date'], '%Y-%m-%d %H:%M:%S')
+            )
+            db.session.add(event)
+            db.session.commit()
+            return event.to_dict(), 201
+        except ValueError as e:
+            return {'error': str(e)}, 400
 
 
 if __name__ == '__main__':

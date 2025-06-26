494bf83d9c4c03ba1f8f9d4a33de1655ae2d951c
Mati Joseph <crucialniccur@gmail.com>
2025-06-26 15:25:35 +0300

    Add initial placeholder component files for Home, MovieList, NavBar, and ReviewForm

---
+++ client/src/components/MovieList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MovieList() {
+  return <h2>Movie List</h2>;
+}
+
+export default MovieList;
---
+++ client/src/components/ReviewForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ReviewForm() {
+  return <form>Review Form</form>;
+}
+
+export default ReviewForm;

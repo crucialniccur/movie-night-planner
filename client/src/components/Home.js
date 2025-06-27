f02949e0204f45615d05046406a16ada412b9c0c
Mati Joseph <crucialniccur@gmail.com>
2025-06-27 17:35:59 +0300

    Refactor Home component to streamline favorite movie functionality and improve poster display

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,9 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [movies, setMovies] = useState([]);
   const apiKey = "a4cd64db16ded6df2896cccfb552989a"; //   Your TMDB key
-
-  const handleFavorite = (movieId) => {
-    fetch(`/favorite/${movieId}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => console.error("Favorite error:", err));
-  };
 
   useEffect(() => {
     fetch(
@@ -25,21 +14,27 @@ function Home() {
       .catch((err) => console.error("Error fetching movies:", err));
   }, [apiKey]);
 
+  const handleFavorite = (movieId) => {
+    fetch(`/favorite/${movieId}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Favorite error:", err));
+  };
+
   return (
     <div className="container">
       <h1>Popular Movies</h1>
       <ul>
         {movies.map((movie) => (
           <li key={movie.id} className="event-card">
-            {movie.poster_path ? (
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={movie.title}
-                className="event-poster"
-              />
-            ) : (
-              <div className="no-poster">No Poster</div>
-            )}
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className="event-poster"
+            />
             <div>
               <h3>{movie.title}</h3>
               <p>{movie.release_date}</p>

7dfd3646dbcf2a51c21f718deeee8654dcfb7f20
Mati Joseph <crucialniccur@gmail.com>
2025-06-27 17:19:16 +0300

    Refactor UserMovie model to include TMDB movie ID and update serialize rules in Review model

--- server/models.py
+++ server/models.py
@@ -33,8 +33,8 @@ class Review(db.Model, SerializerMixin):
     content = db.Column(db.String(500), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
-    serialize_rules = ('-user.reviews', '-event.reviews')
+    movie_id = db.Column(db.Integer, nullable=False)  # TMDB movie ID
+    serialize_rules = ('-user.reviews',)
 
 class UserEvent(db.Model, SerializerMixin):
     __tablename__ = 'user_events'
